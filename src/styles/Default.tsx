0c0aafd25e3709e4ca9ca70b27048c308c1a1b38
hailey-hy <lillyine.hy@gmail.com>
2023-04-29 00:50:28 +0900

    feat: 공통 css 생성 및 적용
    1. 생성한 공통 css 목록
    	- 폰트: 에스코어드림

---
+++ src/styles/Default.tsx
@@ -0,0 +1,15 @@
+import { css } from '@emotion/react';
+
+const Default = css({
+    '@font-face': [
+        {
+            'font-family': 'S-CoreDream-3Light',
+            src: 'url("https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_six@1.2/S-CoreDream-3Light.woff") format("woff")',
+            'font-weight': 'normal',
+            'font-style': 'normal',
+        },
+    ],
+    fontFamily: 'S-CoreDream-3Light',
+});
+
+export default Default;
