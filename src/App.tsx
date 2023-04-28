4e2d4de999325f643f259b6766d8119528920c39
hailey-hy <lillyine.hy@gmail.com>
2023-04-28 23:06:13 +0900

    feat: App에 페이지 컴포넌트 import
    적용된 컴포넌트
    - src/page/Intro
    - src/page/Main

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-import './App.css';
+// import './App.css';
+import Intro from 'pages/Intro';
+import Main from 'pages/Main';
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+    return (
+        <div className='App'>
+            <Intro></Intro>
+            <Main></Main>
+        </div>
+    );
 }
 
 export default App;
