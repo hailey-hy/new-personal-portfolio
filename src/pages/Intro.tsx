d54b24f57cea2d807460a03986e4b894cdce76f8
hailey-hy <lillyine.hy@gmail.com>
2023-04-29 01:50:41 +0900

    feat: Intro 페이지 기본 css 작성
    적용된 css 항목
    - 페이지의 높이를 뷰포트만큼 지정

--- src/pages/Intro.tsx
+++ src/pages/Intro.tsx
@@ -1,9 +1,14 @@
-import React from 'react'
+/** @jsxImportSource @emotion/react */
+import React from 'react';
+import { css } from '@emotion/react';
+
+const IntroStyle = css({
+    width: '100vw',
+    height: '100vh',
+});
 
 const Intro = () => {
-  return (
-    <div>Intro</div>
-  )
-}
+    return <div css={IntroStyle}>Intro</div>;
+};
 
-export default Intro
+export default Intro;
