b78feb146d46e649251f7fe45cdf573e56c8eb4f
hailey-hy <lillyine.hy@gmail.com>
2023-05-03 02:20:39 +0900

    fix: background css 오류 해결

--- src/components/background/Background.tsx
+++ src/components/background/Background.tsx
@@ -3,14 +3,16 @@ import { css } from '@emotion/react';
 import BackgroundImg from './BackgroundImg';
 
 const backgroundStyle = css({
-    width: '80vw',
+    width: '100vw',
     height: '100vh',
     display: 'flex',
     position: 'fixed',
+    background: 'black',
     img: {
         objectFit: 'cover',
         height: '100vh',
         width: '100vw',
+        opacity: 0.8,
     },
 });
 
