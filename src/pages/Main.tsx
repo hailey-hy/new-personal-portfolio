0550b081ad914d7de913752638075d14229a8557
hailey-hy <lillyine.hy@gmail.com>
2023-05-05 13:14:38 +0900

    feat: Section, Article, ArticleDetail 구현
    1. 컴포넌트 상세 설명
    	- Main: 불러올 section에 따라 유형을 props로 전달
    	- Section: 전달받은 유형을 Article에 props로 전달.
    	- Article: 전달받은 유형대로 상수를 불러와 내용을 구현. 그 중 배열은 다시 ArticleDetail에 props로 전달.
    	- ArticleDetail: 전달받은 배열을 구현.
    
    2. 주의 사항
    	- 추상화 및 단일 책임을 위해 컴포넌트를 쪼개었으나, props drilling이 심하진 않은지 확인 필요.

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -2,11 +2,35 @@
 import { useState } from 'react';
 import { css } from '@emotion/react';
 import Nav from 'components/nav/Nav';
+import Section from 'components/article/Section';
+
+const mainStyle = css({
+    display: 'flex',
+    flexDirection: 'row',
+});
+
+const mainSectionStyle = css({
+    display: 'flex',
+    flexDirection: 'column',
+});
+
+export interface sectionType {
+    type: string;
+}
 
 const Main = () => {
+    const [projectProps, setProjectProps] = useState<sectionType>({
+        type: 'PROJECT',
+    });
+    const [articleProps, setArticleProps] = useState<sectionType>({
+        type: 'ARTICLE',
+    });
     return (
-        <div>
-            <Nav></Nav>
+        <div css={mainStyle}>
+            <section css={mainSectionStyle}>
+                <Section sectionProps={projectProps}></Section>
+                <Section sectionProps={articleProps}></Section>
+            </section>
         </div>
     );
 };
