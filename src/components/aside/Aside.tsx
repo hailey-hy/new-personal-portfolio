95f13788433719812bdecee1808a3416d79b063e
hailey-hy <lillyine.hy@gmail.com>
2023-05-16 23:10:33 +0900

    feat: Aside 스타일 수정
    - aside에 직접 스타일을 적용하기 보다, 한차례 내부의 영역에 스타일을 적용하도록 함

--- src/components/aside/Aside.tsx
+++ src/components/aside/Aside.tsx
@@ -1,8 +1,7 @@
 /** @jsxImportSource @emotion/react */
-import { useState, useRef } from 'react';
 import AsideBtnBg from './AsideBtnBg';
 import BtnContent from 'constants/BtnConstants';
-import { asideStyle } from './Style';
+import { asideContainerStyle, asideStyle } from './Style';
 
 export interface AsideBtnType {
     title: string;
@@ -15,20 +14,18 @@ interface AsidePropType {
 }
 
 const Aside = ({ onProjectClick, onActivityClick }: AsidePropType) => {
-    // const [btnContent, setBtnContent] = useState<AsideBtnType[]>(
-    //     BtnContent.Main,
-    // );
-
     return (
         <aside css={asideStyle}>
-            <AsideBtnBg
-                btn={BtnContent.Main[1]}
-                event={onProjectClick}
-            ></AsideBtnBg>
-            <AsideBtnBg
-                btn={BtnContent.Main[2]}
-                event={onActivityClick}
-            ></AsideBtnBg>
+            <div css={asideContainerStyle}>
+                <AsideBtnBg
+                    btn={BtnContent.Main[1]}
+                    event={onProjectClick}
+                ></AsideBtnBg>
+                <AsideBtnBg
+                    btn={BtnContent.Main[2]}
+                    event={onActivityClick}
+                ></AsideBtnBg>
+            </div>
         </aside>
     );
 };
