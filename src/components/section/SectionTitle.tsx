541313a0a469712b43210901f2b6dc97dfed8dae
hailey-hy <lillyine.hy@gmail.com>
2023-07-12 16:30:51 +0900

    Revert "feat: 뷰포트와 컴포넌트간 거리 계산"
    This reverts commit 4dcdbf40c01a3e80443f2694d5ef46c6c8196556.

--- src/components/section/SectionTitle.tsx
+++ src/components/section/SectionTitle.tsx
@@ -1,27 +1,12 @@
 /** @jsxImportSource @emotion/react */
 import { sectionTitleStyle } from './Style';
-import {useState, useEffect, useRef} from 'react'
 
 interface typeType {
     type: string;
 }
 
 const SectionTitle = ({ type }: typeType) => {
-    const sectionRef = useRef<HTMLHeadingElement>(null);
-
-    const distanceFromHTML = () => {
-        if(sectionRef.current){
-            let sectionHeight =
-            sectionRef.current.getBoundingClientRect().top;
-          console.log(sectionRef, sectionHeight);
-        }
-        
-      };
-
-    useEffect(() => {
-        window.addEventListener('scroll', distanceFromHTML)
-    },[])
-    return <h1 css={sectionTitleStyle} ref={sectionRef}>{type}</h1>;
+    return <h1 css={sectionTitleStyle}>{type}</h1>;
 };
 
 export default SectionTitle;
