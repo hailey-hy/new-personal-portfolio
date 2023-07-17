a09f2385bc5a54159d030cff3c2ae255c1c42084
hailey-hy <lillyine.hy@gmail.com>
2023-07-17 09:24:41 +0900

    feat: ArticleDetail 내용 수정

--- src/constants/ArticleConstants.ts
+++ src/constants/ArticleConstants.ts
@@ -50,9 +50,9 @@ export const PROJECT = [
             '재사용 가능한 레이아웃, 컴포넌트 설계',
             'custom hooks를 활용한 단일 책임',
             '타입스크립트 적용',
-            '<https://github.com/hailey-hy/new-personal-portfolio/issues/37>이미지 최적화를 통한 성능 개선',
             '<https://github.com/hailey-hy/new-personal-portfolio/pull/35>github PR, issue 기능을 활용한 작업 내용 기록',
             'airbnb 코드 컨벤션 적용',
+            '<https://github.com/hailey-hy/new-personal-portfolio/issues/37>이미지 최적화를 통한 성능 개선',
             'useRef를 이용한 DOM 접근',
         ],
         tabContents: [
@@ -113,51 +113,40 @@ export const PROJECT = [
             {
                 index: 0,
                 children:
-                    '리액트를 SSR처럼 사용할 수 있습니다. 프로젝트 구조 상 SEO 및 렌더링 속도가 중요한 점, 페이지 이동이 많지 않은 점에 따라 SSR로 개발하고자 선택하였습니다.',
+                    '리액트를 SSR처럼 사용할 수 있습니다. 프로젝트 구조 상 SEO가 중요한 점, 페이지 이동이 많지 않은 점에 따라 SSR로 개발하고자 선택하였습니다.',
             },
+
             {
                 index: 1,
-                children:
-                    'UI를 Component로 나누어 재사용할 수 있어 선택하였습니다. 또한, 리액트에서 제공하는 hooks와 직접 작성하는 custom hooks를 활용하면 개발 효율성을 높일 수 있습니다.',
-            },
-            {
-                index: 2,
                 children:
                     '함수나 객체 안에 들어갈 인자와 프로퍼티의 타입을 추론할 수 있고, 에러를 미리 체크할 수 있습니다. 개발 시 예상치 못한 타입 관련 버그를 방지할 수 있어 선택하였습니다.',
             },
             {
-                index: 3,
+                index: 2,
                 children:
                     'UI 컴포넌트를 독립적으로 구현하고, 테스트할 수 있게 도와줍니다. 특히 Atomic 디자인 패턴에서 유용합니다. 각 단위 컴포넌트들의 props 설정을 한 눈에 볼 수 있어 편리합니다.',
             },
             {
-                index: 4,
+                index: 3,
                 children:
                     '자주 사용하는 CSS를 모아 둔 CSS 프레임워크로, 편리한 사용성으로 개발 속도가 빠르고 유지보수가 쉬워 선택하였습니다.',
             },
             {
                 index: 5,
                 children:
-                    'CSS-in-JS 라이브러리 중 하나이므로 불필요한 클래스 명명을 줄일 수 있고, 타 CSS-in-JS 라이브러리 대비 성능이 빠르고 가벼워 선택하였습니다.',
-            },
-            {
-                index: 6,
-                children:
                     '정해둔 코드 컨벤션에 따라 자동으로 코드를 수정할 수 있어, 일관된 코드 양식을 유지할 수 있어 사용하였습니다.',
             },
             {
-                index: 7,
+                index: 6,
                 children:
                     'github을 기반으로 배포를 도와줍니다. 정해둔 branch에 새로운 push가 생기면 자동으로 배포를 시작하고, lighthouse로 성능 측정도 동시에 할 수 있어 선택하였습니다. ',
             },
         ],
         tabLables: [
             'Next.js',
-            'React',
             'typescript',
             'storybook',
             'tailwind CSS',
-            'Emotion(CSS-in-JS)',
             'Eslint & Prettier',
             'Vercel',
         ],
