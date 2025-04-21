fdd9f9a27a1961eaac8be8b9714b925fcc7dd37b
hailey-hy <lillyine.hy@gmail.com>
2025-04-21 16:38:26 +0900

    fix: Button 내 Icon으로 인한 hydration 에러 해결

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,18 +1,24 @@
 "use client";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Button } from "@/components/ui/button";
 import { useTheme } from "next-themes";
-import { Moon, Sun } from "lucide-react";
+import { Loader, Moon, Sun } from "lucide-react";
 
 const Header = () => {
   const { theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
   const handleClickThemeButton = () => {
     setTheme(theme === "light" ? "dark" : "light");
   };
   return (
-    <div>
-      <Button onClick={handleClickThemeButton}>
-        {theme === "light" ? <Sun /> : <Moon />}
+    <div className="w-full flex justify-end p-3">
+      <Button onClick={handleClickThemeButton} variant="secondary">
+        {!mounted ? <Loader /> : theme === "light" ? <Sun /> : <Moon />}
       </Button>
     </div>
   );
