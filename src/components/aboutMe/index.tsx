3e4d7fbf67b3ba5aa0fc17fd1a5aa73b04776dd0
hailey-hy <lillyine.hy@gmail.com>
2025-05-12 14:48:07 +0900

    feat: 내용 수정

--- src/components/aboutMe/index.tsx
+++ src/components/aboutMe/index.tsx
@@ -1,17 +1,22 @@
 import { ABOUT_ME } from "@/constants/aboutMe";
 import React from "react";
 import Typos from "../common/Typography";
-import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
+import Image from "next/image";
 
 const AboutMe = () => {
   const aboutMe = ABOUT_ME;
   return (
     <div className="flex flex-col mb-8 max-w-[1000px] w-full">
       <div className="flex items-end gap-5 mb-5">
-        <Avatar>
-          <AvatarImage src="/imgs/hailey_cropped.jpg" />
-          <AvatarFallback>CN</AvatarFallback>
-        </Avatar>
+        <div className="relative w-[80px] h-[80px]">
+          <Image
+            src="/imgs/hailey_cropped.jpg"
+            fill
+            alt="hailey"
+            priority
+            className="rounded-full object-cover"
+          />
+        </div>
       </div>
       <Typos typo="h3">{aboutMe.introduction}</Typos>
       <Typos typo="p">{aboutMe.detail}</Typos>
